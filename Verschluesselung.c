b70f4ce6868d4933c5b9a2530df559218b9ecad3
Maximilian <94291040+Maximalst@users.noreply.github.com>
2024-11-12 11:37:53 +0100

    Start

---
+++ Verschluesselung.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("##################################");
+    printf("Wilkommen im Programm der Gruppe 1");
+    printf("##################################");
+
+    return 0;
+
+}

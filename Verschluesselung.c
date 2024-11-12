175cb38b29d6503603c0ee3a29b613c4784ec2d5
Maximilian <94291040+Maximalst@users.noreply.github.com>
2024-11-12 11:46:35 +0100

    Update Verschluesselung.c

--- Verschluesselung.c
+++ Verschluesselung.c
@@ -2,9 +2,9 @@
 
 int main()
 {
-    printf("##################################");
-    printf("Wilkommen im Programm der Gruppe 1");
-    printf("##################################");
+    printf("################################## \n");
+    printf("Wilkommen im Programm der Gruppe 1 \n");
+    printf("################################## \n");
 
     return 0;
 

d7c6b15939f6c29f3f73e0d709ef9e2b51b86be7
Maximilian <stramaxim@gmail.com>
2024-11-14 12:08:26 +0100

    nichts

--- #1 Abgabe.c
+++ #1 Abgabe.c
@@ -21,21 +21,21 @@ void Brute_Force_Komponente() {
 // Menü-Funktion
 int menü() {
     // Startmenü-Ausgabe
-    printf("     ________                   111       \n");    
-    printf("    /     _/                   1111       \n");
-    printf("   |    _/                  1111111       \n");
-    printf("   |   /                   11111111       \n");
-    printf("   |  |   ___      ____        1111       \n");
-    printf("   |  |__|  |     |   /        1111       \n");
-    printf("   |        |     |  |         1111       \n");
-    printf("   |________|     |__|  *      1111       \n");
-    printf("                                          \n");
-    printf("                                          \n");    
-    printf("   Bitte Wählen sie eine Aktion aus:      \n");
-    printf("   [1]  Client-SW                         \n");
-    printf("   [2]  Server-SW                         \n");
-    printf("   [3]  Testumgebung                      \n");
-    printf("   [4]  Brute-Force-Komponente            \n");
+    printf("     ________                   111        \n");    
+    printf("    /     _/                   1111        \n");
+    printf("   |    _/                  1111111        \n");
+    printf("   |   /                   11111111        \n");
+    printf("   |  |   ___      ____        1111        \n");
+    printf("   |  |__|  |     |   /        1111        \n");
+    printf("   |        |     |  |         1111        \n");
+    printf("   |________|     |__|  *      1111        \n");
+    printf("                                           \n");
+    printf("                                           \n");    
+    printf("   Bitte Wählen sie eine Aktion aus:       \n");
+    printf("   [1]  Client-SW                          \n");
+    printf("   [2]  Server-SW                          \n");
+    printf("   [3]  Testumgebung                       \n");
+    printf("   [4]  Brute-Force-Komponente             \n");
 
     // Benutzereingabe
     int eingabe;

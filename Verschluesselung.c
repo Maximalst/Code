9acd132b970c07ef0276bbd826393a928ec01b1d
Maximilian <94291040+Maximalst@users.noreply.github.com>
2024-11-14 11:28:27 +0100

    Update Verschluesselung.c

--- Verschluesselung.c
+++ Verschluesselung.c
@@ -3,6 +3,7 @@
 
 int main()
 {
+        /* Start Menü  */
     printf("     ________                   111       \n");    
     printf("    /     _/                   1111       \n");
     printf("   |    _/                  1111111       \n");
@@ -11,15 +12,19 @@ int main()
     printf("   |  |__|  |     |   /        1111       \n");
     printf("   |        |     |  |         1111       \n");
     printf("   |________|     |__|  *      1111       \n");
-    
     printf("                                          \n");
     printf("                                          \n");    
-
     printf("   Bitte Wählen sie eine Aktion aus:      \n");
     printf("   [1]  Client-SW                         \n");
     printf("   [2]  Server-SW                         \n");
     printf("   [3]  Testumgebung                      \n");
     printf("   [4]  Brute-Force-Komponente            \n");
 
+        /* Programm Auswählen  */
+
+
+    scanf("%d",&zahl); 
+
+    
     return 0;
 }

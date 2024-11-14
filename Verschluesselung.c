03321e0ddca67a442d3fd5d7c7e76b4260748bc9
Maximilian <94291040+Maximalst@users.noreply.github.com>
2024-11-14 09:41:11 +0100

    Update Verschluesselung.c

--- Verschluesselung.c
+++ Verschluesselung.c
@@ -3,9 +3,9 @@
 
 int main()
 {
-    printf("     ______                     111       \n");    
-    printf("    /    _/                    1111       \n");
-    printf("   |    /                   1111111       \n");
+    printf("     _______                    111       \n");    
+    printf("    /     _/                   1111       \n");
+    printf("   |    _/                  1111111       \n");
     printf("   |   /                   11111111       \n");
     printf("   |  |    ___     ____        1111       \n");
     printf("   |  |__|  |     |   /        1111       \n");

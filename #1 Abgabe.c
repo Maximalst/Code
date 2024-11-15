69992658f5641a065e0ae8da1ccd143f5370041b
Maximilian <94291040+Maximalst@users.noreply.github.com>
2024-11-15 21:57:09 +0100

    Update #1 Abgabe.c

--- #1 Abgabe.c
+++ #1 Abgabe.c
@@ -21,11 +21,12 @@ void Brute_Force_Komponente() {
 // Menü-Funktion
 int menü() {
     // Startmenü-Ausgabe
-    printf("     ________                   111        \n");    
-    printf("    /     _/                   1111        \n");
-    printf("   |    _/                  1111111        \n");
-    printf("   |   /                   11111111        \n");
-    printf("   |  |   ___      ____        1111        \n");
+    printf("                                           \n");   
+    printf("     ________                              \n");    
+    printf("    /     _/                  11111        \n");
+    printf("   |    _/                   111111        \n");
+    printf("   |   /                    1111111        \n");
+    printf("   |  |   ___      ____    11111111        \n");
     printf("   |  |__|  |     |   /        1111        \n");
     printf("   |        |     |  |         1111        \n");
     printf("   |________|     |__|  *      1111        \n");

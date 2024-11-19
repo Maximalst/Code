8a7a5b9cb04827a76291ef5a54dbfc61dda3c2d4
Lukas-stff <lsteffen@stud.hs-offenburg.de>
2024-11-19 10:30:05 +0100

    Update 1 Client-SW.c

--- 1 Client-SW.c
+++ 1 Client-SW.c
@@ -1,15 +1,19 @@
+
 #include <stdio.h>
 
 int main() {
     char nachricht[1024];
+    int asciiWerte[1024];
 
-    printf("Hier deine Nachricht eingeben: ");
-    scanf("%s", nachricht); 
+    printf("Hier deine Nachricht eingeben: \n");
+    scanf("%s", nachricht);
 
     printf("Die ASCII-Werte der Nachricht sind:\n");
-    for (int i = 0; nachricht[i] != '\0'; i++) { // Schleife über die Zeichen
-        printf("'%c' -> %d\n", nachricht[i], (int)nachricht[i]);
+    int i;
+    for (i = 0; nachricht[i] != '\0'; i++) {
+        asciiWerte[i] = (int)nachricht[i];
+        printf("'%c' -> %d\n", nachricht[i], asciiWerte[i]); //ASCII Werte sind in asciiWerte[i] gespeichert
     }
-
+    
     return 0;
 }
